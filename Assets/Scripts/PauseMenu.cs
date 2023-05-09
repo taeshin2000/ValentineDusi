48ecabc7327ff591042a91b6a17afac51a7b8bdb
ChawinGowanit <chawingowanit@gmail.com>
2023-05-10 03:27:21 +0700

    add button icon

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool gameIsPaused = false;
     public GameObject pauseMenuUI;
     public GameObject gameOverMenuUI;
+    public Image pauseButton;
+
+    void start(){
+        pauseButton.sprite = Resources.Load<Sprite>("UI/Button/pause");
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -29,6 +36,7 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         gameIsPaused = false;
         FindObjectOfType<GameController>().EnableGameBoard();
+        pauseButton.sprite = Resources.Load<Sprite>("UI/Button/pause");
     }
     public void Pause()
     {
@@ -36,6 +44,8 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 0f;
         gameIsPaused = true;
         FindObjectOfType<GameController>().DisableGameBoard();
+        pauseButton.sprite = Resources.Load<Sprite>("UI/Button/play");
+
     }
 
     public void Toggle(){
