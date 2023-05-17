300004dcdee1fe40f4110db2e74890ca9cbbe887
ChawinGowanit <chawingowanit@gmail.com>
2023-05-17 14:34:07 +0700

    adjust dict page/opening cutscene/change game flow

--- Assets/Scripts/Dict/DictWord.cs
+++ Assets/Scripts/Dict/DictWord.cs
@@ -13,6 +13,15 @@ public class DictWord : MonoBehaviour,IDataPersistence
     [SerializeField] List<WordImage> images = new List<WordImage>();
     [SerializeField] GameObject imageWordPrefabs;
     [SerializeField] TextMeshProUGUI newwordPrefabs;
+    [SerializeField] TextMeshProUGUI collectedDisplay;
+
+    [SerializeField] Button previousButton;
+    [SerializeField] Button nextButton;
+
+    public int currentPage = 1;
+    private int maxPage;
+    private int allword = 0;
+    private int collectedWord = 0;
 
     public void LoadData(GameData gameData){
         this.images = gameData.images;
@@ -22,13 +31,37 @@ public class DictWord : MonoBehaviour,IDataPersistence
     }
     void Start()
     {
+        previousButton.image.gameObject.SetActive(false);
         DataPersistenceManager.instance.LoadGame();
+        maxPage = (int) Mathf.Ceil(images.Count/6f);
         fillData();
+        foreach (var item in images)
+        {
+            foreach (var word in item.words)
+            {
+                allword += 1;
+                if (word.status == "collected"){
+                    collectedWord += 1;
+                }
+            }
+        }
+        collectedDisplay.text = collectedWord.ToString()+"/"+allword.ToString();
     }
 
     void fillData(){
+        while (firstCol.transform.childCount > 0) {
+            DestroyImmediate(firstCol.transform.GetChild(0).gameObject);
+        }
+        while (secondCol.transform.childCount > 0) {
+            DestroyImmediate(secondCol.transform.GetChild(0).gameObject);
+        }
         var isfirst = true;
-        foreach (var item in images) {
+        int index = (currentPage-1)*6;
+        int range = 6;
+        if (currentPage == maxPage){
+            range = images.Count%6;
+        }
+        foreach (var item in images.GetRange(index,range)) {
             GameObject newImage = Instantiate(imageWordPrefabs);
             if (isfirst) {
                 newImage.transform.SetParent(firstCol.transform);
@@ -44,12 +77,51 @@ public class DictWord : MonoBehaviour,IDataPersistence
                 TextMeshProUGUI newText = Instantiate(newwordPrefabs,wordListChild.transform);
                 string temp;
                 if (word.status == "unknown"){
-                    temp = 	"<color=red>"+"???"+"</color> ";
+                    temp = 	"???";
+                }else if(word.status == "found"){
+                    temp = word.word;
+                    newText.color = new Color(150f/255f,150f/255f,150f/255f);
                 }else{
                     temp = word.word;
                 }
                 newText.text = temp;
             }
+        }
+    }
+
+    public void nextPage(){
+        if (currentPage!=maxPage){
+            currentPage += 1;
+            if (currentPage == 1){
+                previousButton.image.gameObject.SetActive(false);
+            }else{
+                previousButton.image.gameObject.SetActive(true);
+            }
+
+            if (currentPage == maxPage){
+                nextButton.image.gameObject.SetActive(false);
+            }else{
+                nextButton.image.gameObject.SetActive(true);
+            }
+            fillData();
+        }
+    }
+
+    public void previousPage(){
+        if (currentPage!=1){
+            currentPage -= 1;
+            if (currentPage == 1){
+                previousButton.image.gameObject.SetActive(false);
+            }else{
+                previousButton.image.gameObject.SetActive(true);
+            }
+            
+            if (currentPage == maxPage){
+                nextButton.image.gameObject.SetActive(false);
+            }else{
+                nextButton.image.gameObject.SetActive(true);
+            }
+            fillData();
         }
     }
 }
